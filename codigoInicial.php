1e344a1d13fba619c22df5f8e2f3695335947113
carlotiii30 <73278607+carlotiii30@users.noreply.github.com>
2023-05-24 16:28:37 +0200

    Cositas - en realidad nada.

---
+++ codigoInicial.php
@@ -0,0 +1,42 @@
+<?php
+
+// - - - - Guardamos la URL de la página actual - - - -
+$pagina_actual = basename($_SERVER['PHP_SELF']);
+
+// - - - Cargamos los mensajes - - -
+$mensajes = json_decode(file_get_contents('./vista/traducciones/traducciones.json'), true);
+
+// - - - - URLs - - - - 
+$enlaces = array(
+    "verIncidencias.php" => "VerIncidencias",
+    "nuevaIncidencia.php" => "NuevaIncidencia",
+    "misIncidencias.php" => "MisIncidencias",
+    "gestionUsuarios.php" => "GestionUsuarios",
+    "log.php" => "Log",
+    "gestionBD.php" => "GestionBBDD"
+);
+
+// - - - Comprobamos si el formulario se ha enviado - - -
+if (isset($_GET) and !empty($_GET)) {
+
+    if (isset($_GET["idioma"]) and isset($_GET["aplicar"])) {
+        $idioma = $_GET["idioma"];
+        setcookie("idioma", $idioma);
+    } else {
+        $idioma = $_COOKIE["idioma"];
+    }
+
+} else {
+    // Inicializamos la variable idioma
+    $idioma = "es";
+}
+
+// - - - Funcion que comprueba si está seleccionado para marcarlo - - - -
+function seleccionado($n, $v)
+{
+    if (isset($_GET[$n]) and ($_GET[$n] == $v))
+        echo 'selected';
+}
+
+
+?>
