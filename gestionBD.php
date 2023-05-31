35400e94f4bb78bc686c6c854c306cbd9ee90b5f
carlotiii30 <73278607+carlotiii30@users.noreply.github.com>
2023-05-31 17:27:48 +0200

    Base de datos

--- gestionBD.php
+++ gestionBD.php
@@ -1,62 +1,59 @@
 <?php
-    require('vista/html/html.php');     // Maquetado de página
-    require('BD/copiaSeguridad.php');   // Backup
+require('vista/html/html.php'); // Maquetado de página
+require('BD/copiaSeguridad.php'); // Backup
 
-    // ************* Inicio de la página
-    htmlStart('Sal y quéjate'); 
-    htmlNavGeneral($mensajes[$idioma]["GestionBBDD"]);
+// ************* Inicio de la página
+htmlStart('Sal y quéjate');
+htmlNavGeneral($mensajes[$idioma]["GestionBBDD"]);
 
-    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $db = conexion();
 
-        $db = conexion();
-        // Opción 1: Descargar copia de seguridad
-        if (isset($_POST["descargar"])) {
-            backup($db);
-        }
-
-        // Opción 2: Restaurar copia de seguridad
-        else if (isset($_POST["restaurar"])) {
-            //restaurar($db, );
-        }
-
-        // Opción 3: Borrar la BBDD (se reinicia)
-        else if (isset($_POST["borrar"])) {
-            borrar($db);
-        }
-
-        desconexion($db);
+    // Opción 1: Descargar copia de seguridad
+    if (isset($_POST["descargar"])) {
+        backup($db);
     }
+
+    // Opción 2: Restaurar copia de seguridad
+    else if (isset($_POST["restaurar"])) {
+        //restaurar($db, );
+    }
+
+    // Opción 3: Borrar la BBDD (se reinicia)
+    else if (isset($_POST["borrar"]) && isset($_POST["confirmar"]) && $_POST["confirmar"] === "si") {
+        borrar($db);
+    }
+
+    desconexion($db);
+}
+
 ?>
-<div class ="gestion">
+
+<div class="gestion">
     <form method="post" action="">
         <!-- Opción 1: Descargar copia de seguridad -->
-        <input type="submit" name="descargar" value="Descargar copia de seguridad">
+        <div class="botones">
+            <input type="submit" name="descargar" value="Descargar copia de seguridad">
 
-        <!-- Opción 2: Restaurar copia de seguridad -->
-        <input type="submit" name="restaurar" value="Restaurar copia de seguridad">
+            <!-- Opción 2: Restaurar copia de seguridad -->
+            <input type="submit" name="restaurar" value="Restaurar copia de seguridad">
 
-        <!-- Opción 3: Borrar la BBDD (se reinicia) -->
-        <input type="submit" name="borrar" value="Borrar la base de datos">
+            <!-- Opción 3: Borrar la BBDD (se reinicia) -->
+            <input type="submit" name="borrar" value="Borrar la base de datos">
+        </div>
+
+        <?php if (isset($_POST["borrar"])) { ?>
+            <div class="seguridad">
+                <p> Esta acción borrará toda la información que hay actualmente en la base de datos. ¿Está seguro/a de que
+                    quiere realizarla? </p>
+                <label for="confirmar"> Escriba "si" para confirmar el borrado: </label>
+                <input type="text" name="confirmar" id="confirmar">
+            </div>
+        <?php } ?>
     </form>
 </div>
 
 <?php
-    htmlAside();
-    htmlEnd();
-
-    /*
-        Hay tres opciones:
-            -  Descargar copia de seguridad.
-                -> Documento que borra y crea cada tabla.
-                -> Inserta los logs.
-                -> Los usuarios.
-                -> Las incidencias.
-
-            - Restaurar copia de seguridad (opcional).
-                -> Página 33 de TW33.
-
-            - Borrar la BBDD (se reinicia)
-                -> Borra y crea cada tabla.
-     */
-
+htmlAside();
+htmlEnd();
 ?>
