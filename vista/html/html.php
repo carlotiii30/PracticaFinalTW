9e3c745bc8b4a8e8924657303a05c2a33b8db456
carlotiii30 <73278607+carlotiii30@users.noreply.github.com>
2023-06-17 18:23:26 +0200

    finiquitao

--- vista/html/html.php
+++ vista/html/html.php
@@ -6,7 +6,6 @@
  */
 
 include "./auxiliar/codigoInicial.php";
-include "./auxiliar/scripts.php";
 
 session_start();
 
@@ -793,7 +792,7 @@ function __formatoIncidencia($incidencia)
     if ($_SESSION['idUsuario'] == $incidencia["idUsuario"] || $_SESSION['rol'] == "admin") {
 
       echo <<<HTML
-            <button type="submit" name="borrar" onmouseover="return confirm('¿Estás seguro de que deseas borrar esta incidencia?')">
+            <button type="submit" name="borrar" onclick="return confirm('¿Estás seguro de que deseas borrar esta incidencia?')">
                 <img src="vista/imagenes/borrar.png">
             </button>
     HTML;
